a727a670bf51e32021fa88921ffd031160c8de24
Priyansh Rajput <priyanshrajput819@gmail.com>
2025-08-28 17:49:06 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     port: 5174,
-    allowedHosts: ["4387a941e543.ngrok-free.app", "4387a941e543.ngrok-free.app", "fe917bf952e5.ngrok-free.app", "6d5602ff9a8a.ngrok-free.app", "93737aa07a54.ngrok-free.app", "fabe2760acf4.ngrok-free.app", "88f5a1b16ad7.ngrok-free.app"],
+    allowedHosts: ["4387a941e543.ngrok-free.app", "4387a941e543.ngrok-free.app", "fe917bf952e5.ngrok-free.app", "6d5602ff9a8a.ngrok-free.app", "93737aa07a54.ngrok-free.app", "fabe2760acf4.ngrok-free.app", "88f5a1b16ad7.ngrok-free.app", "40992d939705.ngrok-free.app"],
   },
 });
