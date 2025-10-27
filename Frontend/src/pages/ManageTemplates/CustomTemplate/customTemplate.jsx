e65c757f81b715ddb6210f9f13a734ef13875a91
Priyansh Rajput <priyanshrajput819@gmail.com>
2025-10-27 22:35:09 +0530

    Updated Custom Template Page

--- Frontend/src/pages/ManageTemplates/CustomTemplate/customTemplate.jsx
+++ Frontend/src/pages/ManageTemplates/CustomTemplate/customTemplate.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
-
+import { customTemplateStyles } from "./customTemplate.styles";
 const CustomTemplate = () => {
   return (
     <>
-      <Box>
-        <Typography variant="h6">Custom Template</Typography>
+      <Box sx={customTemplateStyles.mainContainer}>
+        <Typography variant="h6" sx={{ color: "white", marginBottom: "20px" }}>
+          New Template Message
+        </Typography>
+        <Box sx={customTemplateStyles.formContainer}></Box>
       </Box>
     </>
   );
---
+++ Frontend/src/pages/ManageTemplates/CustomTemplate/customTemplate.styles.js
@@ -0,0 +1,8 @@
+export const customTemplateStyles = {
+  mainContainer: {
+    marginLeft: "240px",
+    paddingTop: "16px",
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+};
